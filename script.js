425d9c800663b8dacc02141fbf213524339bbac6
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-31 09:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
-//your JS code here. If required.
+async function getData(callback){
+	const res = await fetch(url);
+	const data = await res.json();
+	callback(data);
+}
+
+function displayData(data){
+	output.textContent = data.title;
+}
+
+
+btn.addEventListener('click', () => {
+	getData(displayData);
+});
